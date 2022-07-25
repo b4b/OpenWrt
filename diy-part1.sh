eda2320a234b8476e1b1dea51922114f97490990
b4b <3047187@qq.com>
2022-07-25 12:31:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/patches-5.15 target/linux/x86/patches-5.15
+rm -rf target/linux/x86/patches-5.15/.svn
+
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += autocore-x86 lm-sensors-detect kmod-alx kmod-r8125 kmod-vmxnet3 kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-8139cp kmod-8139too kmod-i40e kmod-drm-i915 kmod-mlx4-core kmod-mlx5-core kmod-usb2 kmod-usb3 fdisk lsblk/' target/linux/x86/Makefile
+sed -i 's/kmod-igb /kmod-igb kmod-igb-intel/' target/linux/x86/image/64.mk
+
+mv -f tmp/{r81*,igb-intel} feeds/kiddin9/
+sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
+
+echo '
+CONFIG_CRYPTO_CHACHA20_X86_64=y
+CONFIG_CRYPTO_POLY1305_X86_64=y
+' >> ./target/linux/x86/config-5.15
+
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
